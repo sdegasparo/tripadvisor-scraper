75ddefe03b76a90eef060c507777cc85fa713164
Sven De Gasparo <sven.degasparo@hispeed.ch>
2022-03-21 14:51:58 +0100

    Scrape all hotels and their reviews

--- tripadvisor_scraping/items.py
+++ tripadvisor_scraping/items.py
@@ -9,14 +9,54 @@ from w3lib.html import remove_tags
 import re
 
 
+# Convert month text to digit
+def month_to_digit(month):
+    match month:
+        case "Jan.":
+            return "1"
+        case "Feb.":
+            return "2"
+        case "März":
+            return "3"
+        case "Apr.":
+            return "4"
+        case "Mai":
+            return "5"
+        case "Juni":
+            return "6"
+        case "Juli":
+            return "7"
+        case "Aug.":
+            return "8"
+        case "Sep.":
+            return "9"
+        case "Okt.":
+            return "10"
+        case "Nov.":
+            return "11"
+        case "Dez.":
+            return "12"
+
+
 # Extract the date out of the string
 def extract_review_date(text):
     date = text.replace(' hat im ', '').replace('eine Bewertung geschrieben.', '')
     if date[0].isdigit():
         date = re.sub(r'[0-9+]', '', date).replace('. ', '')
-        date = date + '2022'
+        date = month_to_digit(date)
+        date = str(date) + ' 2022'
+    else:
+        year = date[-5:]
+        month = date[:-5]
+        date = str(month_to_digit(month)) + " " + str(year)
 
     return date
+
+
+# Score is a class like bubble_45 for 4.5, just return the score
+def extract_hotel_score(text):
+    score = text.replace('ui_bubble_rating ', '')
+    return float(score[-2:]) / 10
 
 
 # Score is a class like bubble_50 for 5, just return the score
@@ -24,13 +64,28 @@ def extract_review_score(text):
     return int(text.replace('ui_bubble_rating ', '')[-2])
 
 
+def remove_whitespace(text):
+    return text.strip()
+
+
+class HotelItem(scrapy.Item):
+    hotel_id = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    hotel_name = scrapy.Field(input_processor=MapCompose(remove_tags, remove_whitespace), output_processor=TakeFirst())
+    hotel_score = scrapy.Field(input_processor=MapCompose(remove_tags, extract_hotel_score), output_processor=TakeFirst())
+
+
 class HotelReviewItem(scrapy.Item):
-    # define the fields for your item here like:
+    # hotel_id = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    # hotel_name = scrapy.Field(input_processor=MapCompose(remove_tags, remove_whitespace), output_processor=TakeFirst())
+    review_id = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     username = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     review_helpful_vote = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst()) # TODO Need to get the second element
     review_date = scrapy.Field(input_processor=MapCompose(remove_tags, extract_review_date), output_processor=TakeFirst())
     review_score = scrapy.Field(input_processor=MapCompose(remove_tags, extract_review_score), output_processor=TakeFirst())
-    review_id = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     review_title = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
     review_text = scrapy.Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+
+
+class UserReviewItem(scrapy.Item):
+    pass
 
--- tripadvisor_scraping/spiders/tripadvisorspider.py
+++ tripadvisor_scraping/spiders/tripadvisorspider.py
@@ -1,40 +1,45 @@
 import scrapy
-from tripadvisor_scraping.items import HotelReviewItem
+from tripadvisor_scraping.items import HotelItem, HotelReviewItem, UserReviewItem
 from scrapy.loader import ItemLoader
 
 
 class TripadvisorSpider(scrapy.Spider):
     name = 'tripadvisor'
     # start_urls = ['https://www.tripadvisor.ch/Hotel_Review-g188064-d206048-Reviews-Hotel_des_Balances-Lucerne.html']
-    start_urls = ['https://www.tripadvisor.ch/Hotel_Review-g5237954-d7378599-Reviews-Greuterhof-Islikon_Canton_of_Thurgau.html']
+    start_urls = ['https://www.tripadvisor.ch/Hotels-g910519-Murten_Canton_of_Fribourg-Hotels.html']
 
     def parse(self, response):
-        for hotel_review in response.css('div[data-test-target=reviews-tab]').css('div.cWwQK.MC.R2.Gi.z.Z.BB.dXjiy'):
-            # Need a better solutions then try and except. Needs it, because helpful votes isn't always given
-            # Why does 4 Reviews missing
-            hr = ItemLoader(item=HotelReviewItem(), selector=hotel_review)
+        for hotel in response.css('div.prw_rup.prw_meta_hsx_responsive_listing.ui_section.listItem'):
+            h = ItemLoader(item=HotelItem(), selector=hotel)
+            h.add_css('hotel_id', 'div.meta_listing.ui_columns::attr(data-listingkey)')
+            h.add_css('hotel_name', 'div.listing_title a.property_title.prominent::text')
+            h.add_css('hotel_score', 'a.ui_bubble_rating::attr(class)')
+            yield h.load_item()
 
+            # Go through all hotels on this page
+            hotel_link = hotel.css('div.listing_title a.property_title.prominent::attr(href)').get()
+            if hotel_link is not None:
+                yield response.follow(hotel_link, callback=self.parse_hotel_review)
+
+            # Go to next page
+            next_hotel_page = response.css('a.nav.next.ui_button.primary::attr(href)').get()
+            if next_hotel_page is not None:
+                yield response.follow(next_hotel_page, callback=self.parse)
+
+    def parse_hotel_review(self, response):
+        for hotel_review in response.css('div[data-test-target=reviews-tab] div.cWwQK.MC.R2.Gi.z.Z.BB.dXjiy'):
+            hr = ItemLoader(item=HotelReviewItem(), selector=hotel_review)
+            # TODO also add hotel_id and hotel_name, but the information isn't on this page
+            hr.add_css('review_id', 'div.cqoFv._T::attr(data-reviewid)')
             hr.add_css('username', 'a.ui_header_link::text')
             hr.add_css('review_helpful_vote', 'span.ckXjS::text')
             hr.add_css('review_date', 'div.bcaHz span::text')
             hr.add_css('review_score', 'span.ui_bubble_rating::attr(class)')
-            hr.add_css('review_id', 'div.cqoFv._T::attr(data-reviewid)')
             hr.add_css('review_title', 'a.fCitC span::text')
             hr.add_css('review_text', 'q.XllAv.H4._a span::text')
-
             yield hr.load_item()
 
-                    # 'review_helpful_vote': int(reviews.css('span.ckXjS::text').extract()[1])
-
-                    # 'username': reviews.css('a.ui_header_link::text').get(),
-                    # 'review_helpful_vote': 0,
-                    # 'review_date': self.extractReviewDate(reviews.css('div.bcaHz').css('span::text').get()), # Extract the date out of the string
-                    # 'review_score': reviews.css('span.ui_bubble_rating::attr(class)').get().replace('ui_bubble_rating ', '')[-2], # Score is a class like bubble_50 for 5, just return the score
-                    # 'review_id': reviews.css('div.cqoFv._T::attr(data-reviewid)').get(),
-                    # 'review_title': reviews.css('a.fCitC').css('span::text').get(),
-                    # 'review_text': reviews.css('q.XllAv.H4._a').css('span::text').get(),
-
         # Go to next review page
-        next_page = response.css('a.ui_button.nav.next.primary::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        next_hotel_review_page = response.css('a.ui_button.nav.next.primary::attr(href)').get()
+        if next_hotel_review_page is not None:
+            yield response.follow(next_hotel_review_page, callback=self.parse_hotel_review)
